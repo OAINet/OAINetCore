6acc6dbdc8cf06d88f2d8415b681c624f1b5805b
Baptiste <92639070+S0dlive@users.noreply.github.com>
2024-05-28 21:35:36 +0200

    feat(program): program update.

--- src/OAINet.Node/Program.cs
+++ src/OAINet.Node/Program.cs
@@ -1,17 +1,18 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Nethermind.Libp2p.Stack;
 using OAINet.Node.Network;
 
 public static class Program
 {
-    public static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
         var serviceCollection = new ServiceCollection();
         serviceCollection.AddTransient<Node>();
         serviceCollection.AddLogging(configure => configure.AddConsole())
             .Configure<LoggerFilterOptions>(options => options.MinLevel = LogLevel.Information);
         var builder = serviceCollection.BuildServiceProvider();
-        var app = builder.GetService<Node>();
-        app.RunServer();
+        var node = builder.GetService<Node>();
+        await node.RunNode();
     }
 }
