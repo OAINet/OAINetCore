9c4a59464b561ba60b73b1a369f9d189da1767c7
Baptiste <92639070+S0dlive@users.noreply.github.com>
2024-05-28 21:35:08 +0200

    feat(node): node update.

--- src/OAINet.Node/Network/Node.cs
+++ src/OAINet.Node/Network/Node.cs
@@ -1,19 +1,49 @@
+using System.Net;
+using System.Net.Sockets;
 using Microsoft.Extensions.Logging;
+using Nethermind.Libp2p.Core;
+using Nethermind.Libp2p.Stack;
 
 namespace OAINet.Node.Network;
+
+public record Peer(TcpClient TcpClient, TcpListener TcpListener);
 
 public class Node
 {
     private readonly ILogger<Node> _logger;
+    private Peer _peer;
 
     public Node(ILogger<Node> logger)
     {
         _logger = logger;
+        
     }
 
-    public void RunServer()
+    public async Task RunNode()
     {
-        _logger.LogInformation("server is running");
-        // TODO : server setup.
+        _logger.LogInformation("server is preparing to run");
+        _peer = new Peer(new TcpClient(),
+            new TcpListener(IPAddress.Any, 3024));
+        _peer.TcpListener.Start();
+        _logger.LogInformation("server is waiting external connexion. . .");
+
+        while (true)
+        {
+            try
+            {
+                var client = await _peer.TcpListener.AcceptTcpClientAsync();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.ToString());
+                _logger.LogInformation("server restarting.");
+                _peer.TcpListener.Stop();
+                _peer.TcpListener.Dispose();
+                _peer.TcpClient.Dispose();
+                RunNode();
+                throw;
+            }
+        }
+        Console.ReadKey();
     }
 }
