c113c55baa09907c5e3cb6d29483867b408e9006
Baptiste <92639070+S0dlive@users.noreply.github.com>
2024-06-04 19:33:24 +0200

    feat(wallet): VerifySignature & SignData implementation

--- src/OAINet.Node/Blockchain/Wallet.cs
+++ src/OAINet.Node/Blockchain/Wallet.cs
@@ -1,4 +1,5 @@
 using System.Security.Cryptography;
+using System.Text;
 
 namespace OAINet.Node.Blockchain;
 
@@ -19,6 +20,25 @@ public class Wallet
                 PrivateKey = Convert.ToBase64String(privateKey),
                 PublicKey = Convert.ToBase64String(publicKey)
             };
+        }
+    }
+    public bool VerifySignature(string data, string signature)
+    {
+        using (var ecdsa = ECDsa.Create(ECCurve.NamedCurves.nistP256))
+        {
+            ecdsa.ImportSubjectPublicKeyInfo(Convert.FromBase64String(PublicKey), out _);
+            var hash = SHA256.Create().ComputeHash(Encoding.UTF8.GetBytes(data));
+            return ecdsa.VerifyHash(hash, Convert.FromBase64String(signature));
+        }
+    }
+    
+    public string SignData(string data)
+    {
+        using (var ecdsa = ECDsa.Create(ECCurve.NamedCurves.nistP256))
+        {
+            ecdsa.ImportECPrivateKey(Convert.FromBase64String(PrivateKey), out _);
+            var hash = SHA256.Create().ComputeHash(Encoding.UTF8.GetBytes(data));
+            return Convert.ToBase64String(ecdsa.SignHash(hash)); 
         }
     }
 }
