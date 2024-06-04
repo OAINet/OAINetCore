123646140637ee73eb987b98e3d44885f7d8e2a0
Baptiste <92639070+S0dlive@users.noreply.github.com>
2024-06-04 13:13:28 +0200

    feat(blockchain): blockchain more method.

--- src/OAINet.Node/Blockchain/Block.cs
+++ src/OAINet.Node/Blockchain/Block.cs
@@ -1,3 +1,8 @@
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
 namespace OAINet.Node.Blockchain;
 
 [Serializable]
@@ -7,6 +12,15 @@ public class Block
     public string PreviousHash { get; set; }
     public BaseContentType Content { get; set; }
     public DateTime CreatedAt { get; set; }
+    public string CalculateHash()
+    {
+        using (var sha256 = SHA256.Create())
+        {
+            var rawData = $"{PreviousHash}{CreatedAt}{JsonSerializer.Serialize(Content)}";
+            var bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+            return Convert.ToBase64String(bytes);
+        }
+    }
 }
 
 public abstract class BaseContentType
@@ -15,3 +29,14 @@ public abstract class BaseContentType
 }
 
 public class SimpleContentType : BaseContentType;
+
+public class WalletContentType : BaseContentType
+{
+    public decimal CoinSold { get; set; }
+}
+
+public class TransactionContentType : BaseContentType
+{
+    public string AudiencePK { get; set; }
+    public decimal TransactionSold { get; set; }
+}
--- src/OAINet.Node/Blockchain/Blockchain.cs
+++ src/OAINet.Node/Blockchain/Blockchain.cs
@@ -1,8 +1,11 @@
+using System.Runtime.Serialization.Formatters.Binary;
+
 namespace OAINet.Node.Blockchain;
 
 
 public class Blockchain : IBlockchain
 {
+    private List<Block> pucher = new List<Block>();
     public Block AddBlock(Block block)
     {
         throw new NotImplementedException();
@@ -11,6 +14,40 @@ public class Blockchain : IBlockchain
     public bool CheckIntegrity(List<Block> blockSample)
     {
         throw new NotImplementedException();
+    }
+    
+    private void SaveBlockchain()
+    {
+        if (File.Exists("blockchain.bin"))
+        {
+            // TODO: Save the blockchain with new block in the pusher
+            return;  
+        }
+
+        var safetyBlockchain = RebootBlockchain();
+        using (var stream = File.Create("blockchain.bin"))
+        {
+            BinaryWriter binaryWriter = new BinaryWriter(stream);
+            binaryWriter.Write(safetyBlockchain);
+        }
+    }
+
+    private byte[] RebootBlockchain()
+    {
+        throw new NotImplementedException();
+    }
+
+    private Block CreateGenesisBlock()
+    {
+        return new Block()
+        {
+            PreviousHash = "0",
+            Content = new SimpleContentType()
+            {
+                OwnerPK = "0"
+            },
+            CreatedAt = DateTime.Now
+        };
     }
 }
 
