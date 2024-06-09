55b0499bc9aa584b2b27f26cea73e8682cbb2b41
Baptiste <92639070+S0dlive@users.noreply.github.com>
2024-06-09 18:15:00 +0200

    feat(transaction): TransactionService.cs and WalletService.cs.

---
+++ src/OAINet.Node/Services/Blockchain/TransactionService.cs
@@ -0,0 +1,47 @@
+using Microsoft.Extensions.Logging;
+using OAINet.Node.Blockchain;
+
+namespace OAINet.Node.Services.Blockchain;
+
+public class TransactionService
+{
+    private readonly ILogger<TransactionService> _logger;
+    private readonly Node.Blockchain.Blockchain _blockchain;
+    private readonly WalletService _walletService;
+
+    public TransactionService(
+        ILogger<TransactionService> logger,
+        Node.Blockchain.Blockchain blockchain,
+        WalletService walletService)
+    {
+        _logger = logger;
+        _blockchain = blockchain;
+        _walletService = walletService;
+    }
+
+    public async Task CreateTransaction(WalletCommunicationInformation walletCommunicationInformation,
+        string To,
+        decimal value)
+    {
+        var result = _walletService.VerifySignature(walletCommunicationInformation);
+        if (result != true)
+        {
+            _logger.LogError("a transaction was cancelled because it's not good signature.");
+            return;
+        }
+        var block = new Block()
+        {
+            Content = new TransactionContentType()
+            {
+                FromPk = walletCommunicationInformation.PK,
+                AudiencePK = To,
+                TransactionSold = value
+            }
+        };
+        block = _blockchain.AddBlock(block);
+        var contentType = (TransactionContentType)block.Content;
+        _logger.LogInformation($"{block.Hash} was push in the blockchain, he contain {contentType.TransactionHash} transaction.");
+    }
+}
+
+public record WalletCommunicationInformation(string Data, string PK, string Signature);
--- src/OAINet.Node/Services/Blockchain/WalletService.cs
+++ src/OAINet.Node/Services/Blockchain/WalletService.cs
@@ -1,3 +1,5 @@
+using System.Security.Cryptography;
+using System.Text;
 using Microsoft.Extensions.Logging;
 using OAINet.Node.Blockchain;
 
@@ -16,5 +18,13 @@ public class WalletService
         _blockchain = blockchain;
     }
     
-    
+    public bool VerifySignature(WalletCommunicationInformation walletCommunicationInformation)
+    {
+        using (var ecdsa = ECDsa.Create(ECCurve.NamedCurves.nistP256))
+        {
+            ecdsa.ImportSubjectPublicKeyInfo(Convert.FromBase64String(walletCommunicationInformation.PK), out _);
+            var hash = SHA256.Create().ComputeHash(Encoding.UTF8.GetBytes(walletCommunicationInformation.Data));
+            return ecdsa.VerifyHash(hash, Convert.FromBase64String(walletCommunicationInformation.Signature));
+        }
+    }
 }
