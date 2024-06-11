f4009f4a5d113c94faebc425156c3b8827604143
Baptiste <92639070+S0dlive@users.noreply.github.com>
2024-06-11 14:24:55 +0200

    feat(node-config): Node constants to his configuration.

--- src/OAINet.Node/RequestHandler/Queries/Stocker.cs
+++ src/OAINet.Node/RequestHandler/Queries/Stocker.cs
@@ -1,10 +1,20 @@
+using System.Text.Json;
+using OAINet.Node.Environment;
+using OAINet.Node.Network;
+
 namespace OAINet.Node.RequestHandler.Queries;
 
 public class Stocker
 {
-    [OAINetHandler("hello")]
-    public string HandleWorld(string peerAdress)
+    
+    [OAINetHandler("node/information")]
+    public string QueriesNodeInformation(Request request)
     {
-        return "world, " + peerAdress;
+        return JsonSerializer.Serialize(new
+        {
+            NodeContants.NodePort,
+            NodeContants.BlockchainInjectorTimeInMinute,
+            NodeContants.OAINetProtocolVersion,
+        } );
     }
 }
