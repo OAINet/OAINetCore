b073bb73c49dc268c60759d1cb5de114354f84f7
Baptiste <92639070+S0dlive@users.noreply.github.com>
2024-06-14 21:37:08 +0200

    feat(ioc): node constants has added in ioc.

--- src/OAINet.Node/Environment/NodeContants.cs
+++ src/OAINet.Node/Environment/NodeContants.cs
@@ -1,8 +1,8 @@
 namespace OAINet.Node.Environment;
 
-public static class NodeContants
+public class NodeContants
 {
-    public static readonly string NodePort = System.Environment.GetEnvironmentVariable("OAINET_NODE_PORT");
-    public static readonly string OAINetProtocolVersion = System.Environment.GetEnvironmentVariable("OAINET_PROTOCOL_VERSION");
-    public static readonly string BlockchainInjectorTimeInMinute = System.Environment.GetEnvironmentVariable("OAINET_BLOCKCHAIN_INJECTOR_TIME");
+    public string NodePort { get; private set; } = System.Environment.GetEnvironmentVariable("OAINET_NODE_PORT");
+    public string OAINetProtocolVersion { get; private set; } = System.Environment.GetEnvironmentVariable("OAINET_PROTOCOL_VERSION");
+    public string BlockchainInjectorTimeInMinute { get; private set; } = System.Environment.GetEnvironmentVariable("OAINET_BLOCKCHAIN_INJECTOR_TIME");
 }
--- src/OAINet.Node/Services/OAINetServices.cs
+++ src/OAINet.Node/Services/OAINetServices.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.DependencyInjection;
+using OAINet.Node.Environment;
 
 namespace OAINet.Node.Services;
 
@@ -7,6 +8,7 @@ public static class OAINetServices
     public static void AddOAINetLogicalServices(this IServiceCollection serviceCollection)
     {
         serviceCollection.AddSingleton<Node.Blockchain.Blockchain>();
+        serviceCollection.AddSingleton<NodeContants>();
         serviceCollection.AddTransient<Blockchain.WalletService>();
         serviceCollection.AddTransient<Blockchain.TransactionService>();
     }
