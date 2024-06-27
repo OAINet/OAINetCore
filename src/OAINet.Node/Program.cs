4d95e8180015cab46e28dee1100f768f7a55fdaa
Baptiste <92639070+S0dlive@users.noreply.github.com>
2024-06-27 17:42:21 +0200

    feat(program): GET OUT

--- src/OAINet.Node/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-public static class Program
-{
-    public static async Task Main(string[] args)
-    {
-        
-    }
-}
