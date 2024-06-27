71c0f92d54adeed33c027129291f3838c8196b0d
Baptiste <92639070+S0dlive@users.noreply.github.com>
2024-06-27 17:36:01 +0200

    feat(http): http base.

---
+++ src/Walfu.Node.Logics/WalfuNode.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OAINet.Common.Interfaces;
+
+namespace Walfu.Node.Logics;
+
+public class WalfuNode
+{
+    private readonly IEnumerable<IProtocolSupport> _protocols;
+
+    public WalfuNode(IEnumerable<IProtocolSupport> protocols)
+    {
+        _protocols = protocols;
+    }
+    public void RunNode()
+    {
+        RunProtocols();
+    }
+
+    private void RunProtocols()
+    {
+        foreach (var protocol in _protocols)
+        {
+            protocol.Start();
+        }
+    }
+}
---
+++ src/Walfu.Protocol.HttpsProtocolSupport/HttpsProtocolSupport.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using OAINet.Common.Interfaces;
+
+namespace Walfu.Protocol.HttpsProtocolSupport;
+
+public class HttpsProtocolSupport : IProtocolSupport
+{
+    private IWebHost _webHost;
+    public void Start()
+    {
+        CreateHostBuilder(new []{""}).Build().Run(); ;
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+                webBuilder.UseUrls("https://localhost:3024");
+            });
+    
+    public void Stop()
+    {
+        throw new NotImplementedException();
+    }
+}
